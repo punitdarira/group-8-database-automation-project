d87afb8d8ce0643d0825e4c2381b06488888a66c
Punit Darira <punitdarira@gmail.com>
2024-12-12 22:05:05 -0500

    Fixed date

--- sql/data_seed.sql
+++ sql/data_seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO ClimateData (location, date, temperature, humidity)
 VALUES
     
-    ('Serbia', '2024-18-01', 2.0, 70),
+    ('Serbia', '2024-01-18', 2.0, 70),
     ('Damascus', '2024-01-06', 8.0, 85);
     ('Vienna', '2024-11-11', -9.0, 60),
     ('Mumbai', '2024-07-03', 4.0, 90),
-    ('Bangalore', '2024-27-09', 3.0, 65),
+    ('Bangalore', '2024-09-27', 3.0, 65),
     ('Nerul', '2024-12-12', -5.0, 80);
